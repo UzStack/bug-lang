b5dd0031c7931a151c36d85824544a9cbb595754
A'zamov Samandar <admin@jscorp.uz>
2025-05-10 16:10:24 +0500

    🚀 Yana bir nechta yaxshilanishlar, `row` o'rniga `rows` ni ishlatish va natijani yaxshiroq qaytarish!

--- internal/runtime/std/libs/pgsql.go
+++ internal/runtime/std/libs/pgsql.go
@@ -21,9 +21,34 @@ func PsqlConnect() any {
 }
 
 func Query(db *sql.DB, sql *types.StringValue) any {
-	row, err := db.Query(sql.Value)
+	rows, err := db.Query(sql.Value)
 	if err != nil {
-		panic(err.Error())
+		panic(err)
 	}
-	return row
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err)
+	}
+	var result []any
+	for rows.Next() {
+		values := make([]any, len(columns))
+		pointers := make([]any, len(columns))
+
+		for i := range values {
+			pointers[i] = &values[i]
+		}
+
+		if err := rows.Scan(pointers...); err != nil {
+			panic(err)
+		}
+		res := make(map[string]any)
+		for i, col := range columns {
+			val := values[i]
+			res[col] = val
+		}
+		result = append(result, res)
+	}
+	return result
 }
